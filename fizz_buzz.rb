6337575e167e6c0223c3632dc02218e9510fe70f
fguq <holy.pollyanna@gmail.com>
2019-04-23 21:12:47 +0900

    Finished FizzBuzz Challenge

---
+++ fizz_buzz.rb
@@ -0,0 +1,12 @@
+num = (1..100) 
+num.each do |a|
+  if a % 3 == 0 && a % 5 == 0
+    puts "FizzBuzz"
+  elsif a % 3 == 0
+    puts "Fizz"
+  elsif a % 5 == 0
+    puts "Buzz"
+  else
+    puts a
+  end
+end
